7b32accb3306d2c2ef97449a132259bf49618a8c
Tierney-John <Tierney-John@hotmail.com>
2022-03-17 21:37:30 +0800

    搭建项目基本结构

---
+++ src/main/java/io/github/tierneyjohn/eshow/Application.java
@@ -0,0 +1,20 @@
+package io.github.tierneyjohn.eshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 程序启动类
+ * </p>
+ * <p>create file on 2022/3/17</p>
+ *
+ * @author TierneyJohn
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
